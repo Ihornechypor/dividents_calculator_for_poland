04597f6f0070659a91af24fb5bd1aebe105b1ac7
Ihor Nechypor <chip_a@ukr.net>
2024-01-31 17:47:34 +0100

    upds

--- src/helpers/minusDay.ts
+++ src/helpers/minusDay.ts
@@ -1,5 +1,5 @@
 import { subDays, format } from 'date-fns';
-import { dateFormat } from '../types';
+import { dateFormatTypes } from '../types';
 import { API_DATE_FORMAT } from '../consts';
 
-export const minusDay = (date: dateFormat, day: number) => date && format(subDays(date, day), API_DATE_FORMAT);
+export const minusDay = (date: dateFormatTypes, day: number) => date && format(subDays(date, day), API_DATE_FORMAT);
