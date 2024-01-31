c6be2ca1b35eb8e522964b2c0b39924631e5d417
Ihor Nechypor <chip_a@ukr.net>
2024-01-31 23:00:45 +0100

    uods

--- src/initialStates/index.ts
+++ src/initialStates/index.ts
@@ -10,4 +10,7 @@ export const dividendsReportState = {
   totalTax: '0.00',
   totalTaxPaid: '0.00',
   totalNeedToPay: '0.00',
+  totalGetAll: '0.00',
+  totalPaidFakt: '0.00',
+  totalInHands: '0.00',
 };
